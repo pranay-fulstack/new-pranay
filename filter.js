2a531d9e8281f036dfc8dbcd9176ae5e900e70d0
pranay-fulstack <jyothipranay0437@gmail.com>
2021-01-21 10:46:30 +0530

    filter

---
+++ filter.js
@@ -0,0 +1,15 @@
+let emp=[
+    { name:'pranay',loc:'hyd'},
+    { name:'pavan',loc:'bnglr'},
+    { name:'ahmd',loc:'chennai'},
+    { name:'sandy',loc:'atp'},
+    { name:'teja',loc:'kdp'},
+
+];
+let y=emp.forEach((x)=>x.loc !=='kdp');
+console.log(y);
+    
+
+
+    
+    
