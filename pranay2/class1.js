3dd4ab6b0c4546517eef9dbcf9a3bb289d6b4cbe
pranay-fulstack <jyothipranay0437@gmail.com>
2021-01-26 19:07:05 +0530

    spreadop

---
+++ pranay2/class1.js
@@ -0,0 +1,53 @@
+class Customer{
+    constructor(accNum,accName){
+        this.accNum=accNum;
+        this.accName=accName;
+    
+    console.log("account:",this.accNum,"account",this.accName);}
+}
+    new Customer(1234,"pranay");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
